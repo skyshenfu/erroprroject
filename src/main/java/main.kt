d775bba44fa3d632f73b866addb5f8f75192fa58
painworkpc <zhangty@all-help.com>
2017-06-16 09:26:03 +0800

    upload my error project

---
+++ src/main/java/main.kt
@@ -0,0 +1,18 @@
+/**
+ * Created by zhangty on 2017/6/15.
+ */
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
+
+
+fun main(args: Array<String>) = runBlocking<Unit> {
+    launch(CommonPool) {
+        repeat(1000) { i ->
+            println("I'm sleeping $i ...")
+            delay(500L)
+        }
+    }
+    delay(1300L) // just quit after delay
+}
